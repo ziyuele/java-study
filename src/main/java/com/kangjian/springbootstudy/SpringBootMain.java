44b9710f73e26296452e73382a4b8f98aca59fee
kangjian03 <kangjian03@baidu.com>
2018-10-22 13:56:17 +0800

    update spring boot demo

--- src/main/java/com/kangjian/springbootstudy/SpringBootMain.java
+++ src/main/java/com/kangjian/springbootstudy/SpringBootMain.java
@@ -1,4 +1,31 @@
 package com.kangjian.springbootstudy;
 
+import com.kangjian.springbootstudy.demo.ReadConf;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties
 public class SpringBootMain {
+
+    @Autowired
+    private ReadConf readConf;
+
+    public static void main(String args[]) {
+        SpringApplication.run(SpringBootMain.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println(readConf.getPort());
+            System.out.println(readConf.getHost());
+        };
+    }
+
 }
--- src/main/java/com/kangjian/springbootstudy/demo/ReadConf.java
+++ src/main/java/com/kangjian/springbootstudy/demo/ReadConf.java
@@ -1,4 +1,32 @@
 package com.kangjian.springbootstudy.demo;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+@ConfigurationProperties(prefix = "ss", ignoreUnknownFields = false)
 public class ReadConf {
+    private String host;
+    private int port;
+
+    public int getPort() {
+        return port;
+    }
+
+    public String getHost() {
+        return host;
+
+    }
+
+    public void setPort(int port) {
+
+        this.port = port;
+    }
+
+    public void setHost(String host) {
+
+        this.host = host;
+    }
 }
