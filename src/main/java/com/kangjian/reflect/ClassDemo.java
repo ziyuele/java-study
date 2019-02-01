0768eb791b96cab442f9706cae3c431266f491da
ziyuele <2429362606@qq.com>
2019-02-02 02:02:23 +0800

    boring add

--- src/main/java/com/kangjian/reflect/ClassDemo.java
+++ src/main/java/com/kangjian/reflect/ClassDemo.java
@@ -1,4 +1,12 @@
 package com.kangjian.reflect;
 
 public class ClassDemo {
+
+    private int getAge() {
+       return 0;
+    }
+
+    public int agetAge(String name) {
+       return 1;
+    }
 }
--- src/main/java/com/kangjian/reflect/Main.java
+++ src/main/java/com/kangjian/reflect/Main.java
@@ -1,5 +1,7 @@
 package com.kangjian.reflect;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 public class Main {
     public static void main(String args[]) {
@@ -9,18 +11,29 @@ public class Main {
         Class c = null;
         try {
             c = Class.forName("com.kangjian.reflect.ClassDemo");
-        } catch (ClassNotFoundException e) {
+
+
+            // Class type ---- class type
+            ClassDemo demo = (ClassDemo) c.newInstance();
+        } catch (ClassNotFoundException  | InstantiationException | IllegalAccessException e) {
             //log.error(e.getMessage(), e);
             throw new RuntimeException(e.getMessage(), e);
         }
 
+
         System.out.println(c);
 
+
+        System.out.println("getName:" + c.getName());
+
+        System.out.println("---------------------------");
         /*
          *  2) using Object.class
          */
 
         System.out.println(ClassDemo.class);
+
+        System.out.println("---------------------------");
 
         /*
          *  3) using instance.getClass
@@ -29,6 +42,29 @@ public class Main {
         ClassDemo classDemo = new ClassDemo();
 
         System.out.println(classDemo.getClass());
+
+        System.out.println("---------------------------");
+
+        /**
+         *  4) method reflect and demo
+         */
+
+        Class demo = ClassDemo.class;
+
+        Method methods[] = demo.getMethods();
+
+        for (Method method : methods) {
+            System.out.println("function Name:" + method.getName());
+
+            System.out.println("return type:" + method.getReturnType());
+
+            Class paraTypes[] = method.getParameterTypes();
+
+            for(Class parameter: paraTypes) {
+                System.out.println("parameter:" + parameter);
+            }
+            System.out.println("+++++++++++++++++++++++++++++");
+        }
 
     }
 }
