671ff97caaa65d1679b4ae3376e100bfd557c099
ziyuele <2429362606@qq.com>
2020-08-04 00:50:43 +0800

    feat(nuke-std): add new module for huawei test

---
+++ niuke-std/src/main/java/com/ziyue/niuke/hw/HJ1.java
@@ -0,0 +1,18 @@
+package com.ziyue.niuke.hw;
+
+
+import java.util.Scanner;
+
+/**
+ * 原题出处： https://www.nowcoder.com/practice/8c949ea5f36f422594b306a2300315da?tpId=37&&tqId=21224&rp=1&ru=/ta/huawei/&qru=/ta/huawei/question-ranking
+ *
+ */
+public class HJ1 {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine().trim();
+            System.out.println(line.length() - 1 - line.lastIndexOf(" "));
+        }
+    }
+}
