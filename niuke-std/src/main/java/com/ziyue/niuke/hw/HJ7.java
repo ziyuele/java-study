63edbcf3039f5b9af80e2b104fd8fb67956c560f
ziyuele <2429362606@qq.com>
2020-08-09 02:14:22 +0800

    feat(niuke-std): add HJ7 test case

---
+++ niuke-std/src/main/java/com/ziyue/niuke/hw/HJ7.java
@@ -0,0 +1,23 @@
+package com.ziyue.niuke.hw;
+
+
+/**
+ *  原题见：
+ *  https://www.nowcoder.com/practice/3ab09737afb645cc82c35d56a5ce802a?tpId=37&&tqId=21230&rp=1&ru=/ta/huawei/&qru=/ta/huawei/question-ranking
+ */
+
+import java.util.Scanner;
+
+public class HJ7 {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        float f = scanner.nextFloat();
+        int x = (int) f;
+        if ((f - x) >= 0.5f) {
+            System.out.println(x + 1);
+        } else {
+            System.out.println(x);
+        }
+
+    }
+}
