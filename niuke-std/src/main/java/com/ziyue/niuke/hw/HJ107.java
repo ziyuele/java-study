9e276b48edc6f710cd4373cfcdf528d9519198ed
ziyuele <2429362606@qq.com>
2020-08-29 20:25:49 +0800

    feat(niuke-std): add HJ107 test case

---
+++ niuke-std/src/main/java/com/ziyue/niuke/hw/HJ107.java
@@ -0,0 +1,21 @@
+package com.ziyue.niuke.hw;
+
+import java.util.Scanner;
+
+public class HJ107 {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        double  d       = scanner.nextDouble();
+        System.out.print(getCubeRoot(d, 0d, d));
+    }
+
+    public static double getCubeRoot(double d, double start, double end) {
+        double ret = (start + end) / 2;
+        if ((ret * ret * ret) - d > 0.1d) {
+            return getCubeRoot(d, 0, ret);
+        } else if (d - (ret * ret * ret) > 0.1d) {
+            return getCubeRoot(d, ret, end);
+        }
+        return Math.round(ret * 10) / 10D;
+    }
+}
