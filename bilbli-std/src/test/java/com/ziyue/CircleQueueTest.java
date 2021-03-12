85ee4f5a324cbbf8b7b11427b294b0a409e54c71
ziyuele <2429362606@qq.com>
2021-03-13 01:52:48 +0800

    add circle list

--- bilbli-std/src/test/java/com/ziyue/CircleQueueTest.java
+++ bilbli-std/src/test/java/com/ziyue/CircleQueueTest.java
@@ -1,44 +1,93 @@
 package com.ziyue;
 
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
 /**
- *  数组模拟环形队列的实现
+ * 数组模拟环形队列的实现
  */
 public class CircleQueueTest {
 
-    class CircleQueue {
+    @Test
+    public void testCircleQueue() {
+        CircleQueue circleQueue = new CircleQueue(3);
+        circleQueue.addQueue(1);
+        circleQueue.addQueue(2);
+        System.out.println("size=" + circleQueue.size());
+        circleQueue.addQueue(3);
+
+        System.out.println("size=" + circleQueue.getQueue());
+        circleQueue.addQueue(4);
+        System.out.println(circleQueue.size());
+        System.out.println(circleQueue.getQueue());
+        System.out.println("dataList is: " + circleQueue.getUsageString());
+        System.out.println(circleQueue.getQueue());
+        System.out.println(circleQueue.getQueue());
+        System.out.println("size=" + circleQueue.size());
+    }
+
+    static class CircleQueue {
         private int rear;
         private int front;
         private int[] arr;
-        private int maxSize;
+        private int size;
 
         public CircleQueue(int maxSize) {
-            this.maxSize = maxSize;
+            // 预留一个空间作为约定
+            this.size = maxSize + 1;
             this.rear = 0;
             this.front = 0;
-            this.arr = new int[maxSize];
+            this.arr = new int[size];
         }
 
-        // TODO-kj
         public boolean isEmpty() {
-           return false;
+            return rear == front;
         }
 
-        // TODO-kj
         public boolean isFull() {
-           return false;
+            return (rear + 1) % size == front;
         }
 
-        public void addQueue() {
-
+        public void addQueue(int num) {
+            if (isFull()) {
+                throw new RuntimeException("Queue is full, add failed num=" + num);
+            }
+            arr[rear] = num;
+            rear = (rear + 1) % size;
         }
 
         public int getQueue() {
-           return -1;
+            if (isEmpty()) {
+                throw new RuntimeException("Queue is Empty");
+            }
+            int ret = arr[front];
+            front = (front + 1) % size;
+            return ret;
         }
 
         public int getFirst() {
-           return -1;
+            return arr[front];
+        }
+
+        public int size() {
+            return (rear + size - front) % size;
+        }
+
+        public String toString() {
+            return Arrays.toString(arr);
+        }
+
+        public String getUsageString() {
+            StringBuilder stringBuffer = new StringBuilder();
+            int start = front;
+            int end = rear;
+            while (start != end) {
+                stringBuffer.append(arr[start]).append(" ");
+                start = (start + 1) % size;
+            }
+            return stringBuffer.toString();
         }
     }
 }
